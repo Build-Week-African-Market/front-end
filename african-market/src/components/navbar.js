9fbec6f9a8afbfb5f38e314e6529ffd0026353cc
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-03-24 20:49:47 -0400

    fixed basic structure of navbar

---
+++ african-market/src/components/navbar.js
@@ -0,0 +1,43 @@
+
+
+function NavBar(props) {
+// RETURNS NAVBAR AT TOP OF SCREEN
+// LOGO TO THE FAR LEFT LINKS TO HOME
+// LOGIN/SIGNUP BUTTONS ON FAR RIGHT
+// ABOVE BUTTONS CHANGE TO DROPDOWN WHEN LOGGED IN
+// DROPDOWN MENU ALLOWS LOGOUT OR ADD ITEMS
+
+// PROPS TO DETERMINE TO RENDER LOG IN BUTTONS OR DROPDOWN MENU
+// let {loginBoolean} = props
+
+    return (
+        <div>
+            <button />
+            {/* TERNARY CHECKS WHETHER OR NOT SELLER IS LOGGED IN */}
+            {/* IF SELLER IS LOGGED IN RENDER DROPDOWN MENU */}
+            {/* IF NOT LOGGED IN RENDER LOGIN IN BUTTONS */}
+                {/* {loginBoolean ? 
+                <div>    
+                    <label>
+                        USERNAME
+                        <select>
+                            <option>
+                                Add new Item
+                            </option>
+                            <option>
+                                Logout
+                            </option>
+                        </select>
+                    </label>
+                </div> :
+                <div>  
+                    <button>
+                        Log-in
+                    </button>
+                    <button>
+                        Sign-up
+                    </button>
+                </div>} */}
+        </div>
+    )
+}
